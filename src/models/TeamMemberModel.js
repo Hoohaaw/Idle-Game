6abebe8e9e75f42a5836eb6b4c8dc4651aca12e8
Alex Palm <ap224gy@student.lnu.se>
2025-04-25 09:27:43 +0200

    Add TeamMemberModel file for future use in team

---
+++ src/models/TeamMemberModel.js
@@ -0,0 +1,35 @@
+import { name } from "ejs";
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+const teamMemberSchema = new Schema({
+
+    userID: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    attack: {
+        type: Number,
+        required: true
+    },
+    health: {
+        type: Number,
+        required: true
+    },
+    class: {
+        type: String,
+        required: true
+    },
+    level: {
+        type: Number,
+        required: true
+    },
+    experience: {
+        type: Number,
+        required: true
+    }
+})
