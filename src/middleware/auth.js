22bf487bb49a115a64f87df56ab59a0eed12d24c
Alex Palm <ap224gy@student.lnu.se>
2025-04-30 18:25:41 +0200

    Remove unneccessary comments

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,12 +1,9 @@
 function authUser(req, res, next) {
     if (req.session && req.session.user && req.session.user.username) {
-        // res.status(200).send('Authorized')
        next();
     } else {
         req.session.flash = { type: 'danger', text: 'Not authorized, try to login!' }
         res.redirect('/login')
-        // res.status(401).json({ message: 'Not authorized' })
-     
     }
 }
 
